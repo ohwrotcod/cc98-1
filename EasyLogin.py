54f8ecb019449f90dc6576e8d2372bddde3d54c6
zjuchenyuan <chenyuan@zju.edu.cn>
2016-11-30 21:32:38 +0800

    upgrade EasyLogin

--- EasyLogin.py
+++ EasyLogin.py
@@ -1,15 +1,20 @@
-# coding:gbk
+# coding:utf-8
+#TODO:
+#multiple User-Agent; support for json object post; support file upload
+#more document is also needed
+#UPDATE_LOG:
+#2016/11/25
+#adding VIEWSTATE,save,load function
 #2016/10/28
-#Ä¬ÈÏresult=True,É¾³ýfrom_encoding=x.encoding
+#"result=True" by default, delete "from_encoding=x.encoding"
 
-import requests
+import requests,pickle
 from bs4 import BeautifulSoup
 try:
     from urllib.parse import urlencode
 except:
     print("Please Use Python3")
     exit()
-import pickle
 
 class EasyLogin():
     def __init__(self,cookie="",cookiefile=None,proxy=None):
@@ -32,25 +37,31 @@ class EasyLogin():
             c+= i.name+"="+i.value+";"
         return c
         
-    def get(self,url,result=True):
-        x = self.s.get(url,allow_redirects=False,proxies=self.proxies)
+    def get(self,url,result=True,save=False,headers=None,o=False):
+        x = self.s.get(url,headers=headers,allow_redirects=False,proxies=self.proxies)
         if result: 
             if 'Location' in x.headers or len(x.text)==0: return False
             else:self.b = BeautifulSoup(x.text.replace("<br>","\n").replace("<BR>","\n"),'html.parser')
-        return x.text
-    
-    def post_dict(self,url,dict,result=False,save=False):
+        if save: open(self.cookiefile,"wb").write(pickle.dumps(self.s.cookies))
+        if o:#if you need object returned
+            return x
+        else:
+            return x.text
+
+    def post(self,url,data,result=True,save=False,headers=None):
+        postheaders={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        if headers is not None:
+            #postheaders.update(headers)
+            postheaders=headers
+        #print(data)
+        x = self.s.post(url,data,headers=postheaders,allow_redirects=False,proxies=self.proxies)
+        if result: self.b = BeautifulSoup(x.text,'html.parser')
+        if save:  open(self.cookiefile,"wb").write(pickle.dumps(self.s.cookies))
+        return x
+
+    def post_dict(self,url,dict,result=True,save=False,headers=None):
         data = urlencode(dict)
-        x = self.s.post(url,data,headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},allow_redirects=False,proxies=self.proxies)
-        if save:  open(self.cookiefile,"wb").write(pickle.dumps(self.s.cookies))
-        if result: self.b = BeautifulSoup(x.text,'html.parser')
-        return x
-    
-    def post(self,url,data,result=False,save=False):
-        x = self.s.post(url,data,headers={'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},allow_redirects=False,proxies=self.proxies)
-        if result: self.b = BeautifulSoup(x.text,'html.parser')
-        if save:  open(self.cookiefile,"wb").write(pickle.dumps(self.s.cookies))
-        return x
+        return self.post(url,data,result=result,save=save,headers=headers)
     
     def f(self,name,attrs):#find_all
         if self.b == None: return []
@@ -64,3 +75,22 @@ class EasyLogin():
             if searchString in element.get(searchTarget,""):
                 result.append(element[returnType] if returnType!="element" else element)
         return result
+     
+    def VIEWSTATE(self):
+         if self.b == None: return ""
+         x = self.b.find("input",attrs={"name":"__VIEWSTATE"})
+         if x == None: return ""
+         return x["value"]
+         
+    def save(self,filename="EasyLogin.status"):
+        open(filename,"wb").write(pickle.dumps(self))
+    
+    def load(filename="EasyLogin.status"):
+        return pickle.load(open(filename,"rb"))
+        
+if __name__ == '__main__':#sample code for get ip by "http://ip.cn"
+    a = EasyLogin()
+    page =a.get("http://ip.cn")
+    IP,location = a.f("code",attrs={})
+    print(IP)
+    print(location)
